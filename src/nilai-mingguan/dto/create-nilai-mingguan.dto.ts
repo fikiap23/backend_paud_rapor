3c06e47e35f81e211f82b791ac195f9c2aecb62d
fikiaprian23 <fikiaprian23@gmail.com>
2024-03-24 07:37:25 +0700

    setup module nilai mingguan

---
+++ src/nilai-mingguan/dto/create-nilai-mingguan.dto.ts
@@ -0,0 +1,24 @@
+import { Nilai } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePenilaianMingguanDto {
+    @IsEnum(Nilai)
+    @IsNotEmpty()
+    nilai: Nilai;
+
+    @IsString()
+    @IsNotEmpty()
+    deskripsi: string;
+
+    @IsString()
+    @IsNotEmpty()
+    idModulAjar: string;
+
+    @IsString()
+    @IsNotEmpty()
+    idMurid: string;
+
+    @IsString()
+    @IsNotEmpty()
+    idMapel: string;
+}
